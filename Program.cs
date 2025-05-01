daea5c396da299850fbf46043565a6160884e1dc
andre <nyxwallestein@gmail.com>
2025-05-01 17:10:21 -0500

    ejercicio1

--- Program.cs
+++ Program.cs
@@ -7,7 +7,42 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("bienvenido al curso de c#");
+            int aprobados = 0;
+            int desaprobados = 0;
+            int exonerados = 0;
+
+            Console.Write("Ingrese la cantidad de alumnos: ");
+            int cant = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= cant; i++)
+            {
+                Console.Write($"Ingrese la nota final del alumno (0 a 20): ");
+                int nota = int.Parse(Console.ReadLine());
+
+                if (nota >= 0 && nota <= 20)
+                {
+                    if (nota >= 18)
+                    {
+                        exonerados++;
+                    }
+                    else if(nota >= 10 )
+                       {
+                        aprobados ++;
+                    }
+                    else
+                    {
+                        desaprobados++;
+                    }
+                }
+
+            }
+
+            Console.WriteLine("\nResultados:");
+            Console.WriteLine("aprobados: " + aprobados);
+            Console.WriteLine("desaprobados: "+ desaprobados);
+            Console.WriteLine("exonerados: " + exonerados);
+
+        }
         }
     }
-}
+
